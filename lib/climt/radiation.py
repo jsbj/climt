10607fdb1c6fc66cc932dc6a267b8c8eb202590c
Jonah Bloch-Johnson <me@jonah.org>
2013-07-17 10:54:46 -0500

    beginning rrtm work

--- lib/climt/radiation.py
+++ lib/climt/radiation.py
@@ -80,7 +80,7 @@ class radiation(Component):
     def __init__(self, scheme = 'ccm3', **kwargs):
         # Initialize scheme-dependent attributes
         if scheme in ['gray','grey','graygas']: scheme='greygas'
-        if scheme not in ['greygas','chou','ccm3','cam3']:
+        if scheme not in ['greygas','chou','ccm3','cam3', 'rrtm']:
             raise ValueError,'\n \n ++++ CliMT.radiation: Scheme %s unknown' % scheme
         exec('self.__%s__init__()' % string.lower(scheme))
 
@@ -168,6 +168,73 @@ class radiation(Component):
         self.SteppingScheme = 'explicit'
         self.ToExtension    = ['beta_greygas','stebol','g','dt','Cpd','tau_inf','alpha_greygas','T','q','p','ps','solin','Ts']
         self.FromExtension  = ['Tinc','TdotRad','SrfRadFlx','swhr','swflx','lwhr','lwflx','lwup','lwdown','lwtau']
+        self.Required       = ['T','q','p','ps','solin','Ts']
+        self.Prognostic     = ['T']
+        self.Diagnostic     = ['TdotRad','SrfRadFlx','lwhr','lwflx','lwup','lwdown','lwtau']
+    
+    def __rrtm__init__(self):
+        # Load extension
+        try: import _rrtm_radiation
+        except: raise ImportError, '\n \n ++++ CliMT.radiation: Could not load rrtm scheme'
+        # Define some attributes
+        self.name = 'rrtm_radiation'
+        self.LevType = 'p'
+        self.Extension = _rrtm_radiation
+        self.driver = _rrtm_radiation.driver
+        self.SteppingScheme  'explicit'
+        self.ToExtension = [
+            'do_sw', #     0  Shortwave switch     (integer)       1   1 / 0 => do / do not compute SW        
+            'do_lw', #     0  Longwave switch      (integer)       1   1 / 0 => do / do not compute LW       
+            'scon', #      0  Solar constant       W m-2        1367.
+            'orb_yr', #    0  Orbital year         (integer)    1995   Year used to compute orbital params
+            'avg', #       0  Insolation average   (string)   'daily'  Choices are: 'inst', 'daily', 'annual'
+            'co2', #       0  CO2                  ppmv          330.
+            'n2o', #       0  N2O                  ppmv            0.
+            'ch4', #       0  CH4                  ppmv            0.
+            'cfc11', #     0  CFC11                ppmv            0.
+            'cfc12', #     0  CFC12                ppmv            0.
+            'cfc22', #     0  CFC22                ppmv            0.   Chou scheme only
+            'tauvis', #    0  Aerosol opt. depth   (float)         0.   CCM3 only
+            'tau_inf', #   0  Total opt. depth        -            1.   Greygas scheme only
+            'alpha_greygas', # 0  Tau shape parameter   -          1.   Greygas scheme only  
+            'calday', #    0  Calendar day         (float)      80.5    Insolation computed at specified
+            'lat', #     0-1  Latitude                 dgr      0.         day and lat/lon if  solin 
+            'lon', #     0-1  Longitude                dgr      0.         and zen are NOT specified
+            'solin', #   0-2  Insolation               W/m2     417.4   Daily-mean on equator at equinox
+            'zen', #     0-2  Solar zenith angle       dgr      72.2    Daily-mean on equator at equinox
+            'Ts', #      0-2  Surface temperature      K        283.15
+            'ps', #      0-2  Surface pressure         mb       1000.
+            'aldif', #   0-2  Diffuse IR albedo        (frac)   0.07
+            'aldir', #   0-2  Direct IR albedo         (frac)   0.07
+            'asdif', #   0-2  Diffuse UV+vis alb       (frac)   0.07
+            'asdir', #   0-2  Direct UV+vis alb        (frac)   0.07
+            'p', #       1-3  Atmospheric pressure     mb               Default is equispaced 0-ps. p[0] is top level
+            'T', #       1-3  Temperature              K        283.15  Isothermal
+            'q', #       1-3  Specific humidity        g/kg     1.e-5
+            'o3', #      1-3  Ozone mass mix. rat.     kg/kg            Default obtained by interpolating a tropical data profile
+            'cldf', #    1-3  Cloud fraction           frac     0.
+            'r_liq', #   1-3  Drop radius, liquid      micron   10.   
+            'r_ice', #   1-3  Drop radius, ice         micron   30.   
+            'clwp', #    1-3  Cloud liquid water path  g/m2     0.     
+            'ciwp', #    1-3  Cloud ice water path     g/m2     -99.   If not passed explicitly, ice frac computed internally (CAM3 only)
+            'in_cld', #    0  Cloud water path flag     -       0       0 / 1 => grid avg / in-cloud water paths (CAM3 only)
+            'flus' #    1-3  Upwelling LW at surface  W/m2     -99.   If not passed explicitly, computed from Ts using emiss=1 (CAM3 only)
+        ]
+        
+        self.FromExtension = [
+            'swhr', #       SW heating rate                 K day-1
+            'lwhr', #       LW heating rate                 K day-1
+            'swflx', #      Net SW radiative flux           W m-2   At grid-cell midpoints
+            'lwflx', #      Net LW radiative flux           W m-2   At grid-cell midpoints
+            'SwToa', #      Top-of-atmos SW rad flux        W m-2
+            'LwToa', #      Top-of-atmos LW rad flux        W m-2
+            'SwSrf', #      Surface  SW rad flux            W m-2
+            'LwSrf', #      Surface  LW rad flux            W m-2
+            'SwToaCf', #    SW cloud forc, top of atmos     W m-2   
+            'SwSrfCf', #    SW cloud forc, surface          W m-2   
+            'LwToaCf', #    LW cloud forc, top of atmos     W m-2   
+            'LwSrfCf' #    LW cloud forc, surface          W m-2
+        ]
         self.Required       = ['T','q','p','ps','solin','Ts']
         self.Prognostic     = ['T']
         self.Diagnostic     = ['TdotRad','SrfRadFlx','lwhr','lwflx','lwup','lwdown','lwtau']
